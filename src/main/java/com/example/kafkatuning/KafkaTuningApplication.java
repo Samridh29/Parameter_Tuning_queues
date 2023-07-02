6ac71f1acc7a6f00538b814d44ab7d14dfb68f23
Samridh29 <samridhagarwal011@gmail.com>
2023-07-03 02:05:43 +0530

    Init

---
+++ src/main/java/com/example/kafkatuning/KafkaTuningApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkatuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTuningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkatuning/KafkaTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTuningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
