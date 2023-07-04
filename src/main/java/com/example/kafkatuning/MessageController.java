8361196f3e55698d397c06d58fe6e163607ff9ad
Samridh29 <samridhagarwal011@gmail.com>
2023-07-04 17:57:33 +0530

    API

---
+++ src/main/java/com/example/kafkatuning/MessageController.java
@@ -0,0 +1,26 @@
+package com.example.kafkatuning;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public String publish(@RequestBody MessageRequest messageRequest){
+        kafkaTemplate.send("Samridh", messageRequest.message());
+        return "Published Successfully";
+
+    }
+
+}
---
+++ src/main/java/com/example/kafkatuning/MessageRequest.java
@@ -0,0 +1,4 @@
+package com.example.kafkatuning;
+
+public record MessageRequest(String message) {
+}
